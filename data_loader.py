07bf7bfaba2a4af445cfd600b188d94f4e907724
Alpha-Kittens <90939705+Alpha-Kittens@users.noreply.github.com>
2022-10-31 17:03:42 -0400

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,19 +1,38 @@
 import re
+import os
 
 digit = r'\d+'
 
 def get_metadata(file):
-    name, ext = file.split(".")
+    name, ext = file.split('.')
     if ext == "Chn":
         return None
-    unprocessed = name.split("_")
-    iteration = 1 if len(unprocessed) == 2 else re.search(digit, unprocessed[2]).group(0)
+    unprocessed = name.split('_')
+    iteration = 1 if len(unprocessed) == 2 else int(re.search(digit, unprocessed[2]).group(0))
     return unprocessed[0], unprocessed[1], iteration
 
-def read_data(file, return_hist = False):
+def read_data(file):
     
+    data = {}
     metadata = get_metadata(file)
     if metadata is None:
         return None
+    data['target'], data['angle'], data['iteration'] = metadata
+    data['time'], data['histogram'] = get_file_info(file)
+    data['counts'] = sum(data['histogram'])
+    data['cps'] = data['counts'] / data['time']
 
+    return data
+
+def add_data(data, file):
+
+    entry = read_data(file)
+    data[(entry['target'], entry['angle'], entry['iteration'])] = entry['time'], entry['histogram']
+
+def recursive_read(data, folder):
     
+    for entry in os.listdir(folder):
+        if os.path.isfile(os.path.join(entry, folder)):
+            add_data(data, entry)
+        else:
+            recursive_read(data, entry)
