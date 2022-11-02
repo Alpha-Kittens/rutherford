4f5437a707e16ad7e9fde25629bd31fe191a5bb0
Alpha-Kittens <90939705+Alpha-Kittens@users.noreply.github.com>
2022-11-02 16:08:42 -0400

    Update models.py

--- models.py
+++ models.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+def const_cross_section(c, theta):
+    return c / (np.sin(theta))**4
+    
 def cross_section(Z, E, theta, Z_alpha = 2, e = 1):
     return (Z * Z_alpha * e**2 / 4*E)**2 / (np.sin(theta))**4
 
